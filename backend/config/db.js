29b465b81925f5da0180949d23752da27ae3c6f8
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-08-02 22:02:44 -0400

    deploy

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,9 +7,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/GameSaver', {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
+      useUnifiedTopology: true
     }) ;
     console.log(
       `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
