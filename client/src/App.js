ece292e566b28ddbe7a8f006b55d532e1e5b6327
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-08-02 22:17:26 -0400

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -50,15 +50,15 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem('id_token');
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
