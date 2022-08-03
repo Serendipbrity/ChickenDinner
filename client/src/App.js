0b7ad3086b1fd7275dde4f6aea30e44f894c4901
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-08-02 22:14:24 -0400

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -46,10 +46,29 @@ const cache = new InMemoryCache({
 }
 );
 
-const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
-  cache,
+const httpLink = createHttpLink({
+  uri: '/graphql',
 });
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+// const client = new ApolloClient({
+//   uri: 'http://localhost:3000/graphql',
+//   cache,
+// });
 
 function App() {
   return (
