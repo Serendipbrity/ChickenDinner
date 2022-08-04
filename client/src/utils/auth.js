f2ffb472a77a4ba54888a8f06e994a0f88fbe495
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-08-04 19:38:23 -0400

    scripts

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,59 +1,59 @@
-const decode = require( 'jwt-decode');
+// // const decode = require( 'jwt-decode');
 
-// const jwt = require('jsonwebtoken');
+// // const jwt = require('jsonwebtoken');
 
-const secret = process.env.JWT_SECRET;
-const expiration = '2h';
+// const secret = process.env.JWT_SECRET;
+// const expiration = '2h';
 
-// module.exports = {
-//   signToken: function ({ username, email, id }) {
-//     const payload = { username, email, id };
+// // module.exports = {
+// //   signToken: function ({ username, email, id }) {
+// //     const payload = { username, email, id };
 
-//     return jwt.sign({data:payload}, secret, { expiresIn: expiration });
+// //     return jwt.sign({data:payload}, secret, { expiresIn: expiration });
+// //   }
+// // };
+
+// class AuthService {
+//   getProfile() {
+//     return decode(this.getToken());
 //   }
-// };
 
-class AuthService {
-  getProfile() {
-    return decode(this.getToken());
-  }
+//   loggedIn() {
+//     // Checks if there is a saved token and it's still valid
+//     const token = this.getToken();
+//     return !!token && !this.isTokenExpired(token); // handwaiving here
+//   }
 
-  loggedIn() {
-    // Checks if there is a saved token and it's still valid
-    const token = this.getToken();
-    return !!token && !this.isTokenExpired(token); // handwaiving here
-  }
+//   isTokenExpired(token) {
+//     try {
+//       const decoded = decode(token);
+//       if (decoded.exp < Date.now() / 1000) {
+//         return true;
+//       } else return false;
+//     } catch (err) {
+//       return false;
+//     }
+//   }
 
-  isTokenExpired(token) {
-    try {
-      const decoded = decode(token);
-      if (decoded.exp < Date.now() / 1000) {
-        return true;
-      } else return false;
-    } catch (err) {
-      return false;
-    }
-  }
+//   getToken() {
+//     // Retrieves the user token from localStorage
+//     return localStorage.getItem('id_token');
+//   }
 
-  getToken() {
-    // Retrieves the user token from localStorage
-    return localStorage.getItem('id_token');
-  }
+//   login(idToken) {
+//     // Saves user token to localStorage
+//     localStorage.setItem('id_token', idToken);
 
-  login(idToken) {
-    // Saves user token to localStorage
-    localStorage.setItem('id_token', idToken);
+//     window.location.assign('/');
+//   }
 
-    window.location.assign('/');
-  }
+//   logout() {
+//     // Clear user token and profile data from localStorage
+//     // axios.defaults.headers.common["Authorization"] = null;
+//     localStorage.removeItem('id_token');
+//     // this will reload the page and reset the state of the application
+//     window.location.assign('/');
+//   }
+// }
 
-  logout() {
-    // Clear user token and profile data from localStorage
-    // axios.defaults.headers.common["Authorization"] = null;
-    localStorage.removeItem('id_token');
-    // this will reload the page and reset the state of the application
-    window.location.assign('/');
-  }
-}
-
-module.exports =  new AuthService();
+// module.exports =  new AuthService();
