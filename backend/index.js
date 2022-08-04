d6e5345e317e715b4b9c337ef9aa778ca12bd0d6
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-08-04 18:29:32 -0400

    deploy

--- backend/index.js
+++ backend/index.js
@@ -33,4 +33,9 @@ app.use(
   })
 );
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+};
+
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
